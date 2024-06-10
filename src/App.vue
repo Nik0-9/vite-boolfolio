c3e81f5a33605d0dcab6f36dbacae0dbb95e5c48
Nik0-9 <filannino.nicola9@gmail.com>
2024-06-10 18:43:49 +0200

    (App.vue) ADDED methods: getAllProject

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <h1 class="text-center">Ciao</h1>
-  <i class="fa fa-solid fa-home"></i>
+  <h1 class="text-center">Project</h1>
+  <ul>
+     <li v-for="project in projects" :key="project.id">
+      {{project.title}}
+    </li> 
+  </ul>
 </template>
 
 <script>
+import {store} from './store';
+import axios from 'axios';
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        store,
+        projects:[]
+      }
+    },
+    methods:{
+      getAllProjects() {
+        axios.get(this.store.apiBaseUrl + '/projects').then((resp) => {
+          console.log(resp.data);
+          this.projects = resp.data.results.data;
+        })
+      }
+    },
+    mounted() {
+      this.getAllProjects();
+    }
   }
 </script>
 
